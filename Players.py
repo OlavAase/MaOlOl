82338d7d510d32fb5dc7649e275e238136cf2734
Magnus44 <ingulmagnus@gmail.com>
2024-11-05 14:22:20 +0100

    definert en klasse for spiller

--- Players.py
+++ Players.py
@@ -1,1 +1,7 @@
 #Her lager vi classene med spillerne og computerne
+
+class Spiller:
+    def __init__(self, navn, antall_terninger, nummer):
+        self.navn = navn
+        self.antall_terninger = antall_terninger
+        self.nummer = nummer
