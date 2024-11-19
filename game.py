9a3cd6318b46b8114a94ef554e3d99502e3a866d
Olav45 <oivarrud@gmail.com>
2024-11-19 14:58:46 +0100

    Starta på game class. Man kan gjette så lenge det er mer en current guess

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
 class Game: 
-    pass
+    def __init__(self, players):
+        self.players = players
+        self.current_guess = {
+            "die_face": 4,
+            "amount_of_dice": 7,
+        }
 
+    def guess(self, face, amount):
+        if face > self.current_guess["die_face"] or amount > self.current_guess["amount_of_dice"]:
+            self.current_guess["die_face"] = face
+            self.current_guess["amount_of_dice"] = amount
+            return self.current_guess
+        else:
+            return "Ble ikke akseptert"
+
+
+player1 = Game("player1")
+print(player1.guess(4, 6))
