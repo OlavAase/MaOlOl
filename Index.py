63c6401f0ff0a0bd2775c066137038f9a1dceb5a
OlavAase <162561583+OlavAase@users.noreply.github.com>
2024-12-04 19:58:17 +0100

    enda mindre i main

--- Index.py
+++ Index.py
@@ -1,11 +1,8 @@
 ###Her 
 
-from player import Player
 from random import randint as rd
-from game import Game, gameloop
+from game import gameloop
 from gamestartfunksjon import gamestart
-
-           
 
 gamestart()
 gameloop()
